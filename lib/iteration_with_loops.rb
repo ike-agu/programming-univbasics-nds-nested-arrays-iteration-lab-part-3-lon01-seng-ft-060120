50c35769f92eb3a8beb537ace2517792eb9a7619
ikenna agulobi <ikennaww@yahoo.fr>
2020-04-22 00:22:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,3 +1,5 @@
+
+require "pry"
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
@@ -7,7 +9,7 @@ new_array = []
 while row_index < mixed_data.count do
   element_index = 0
   string_value = (" ")
-
+binding.pry
   while element_index < src[row_index].count do
 
     p src[row_index][element_index]
